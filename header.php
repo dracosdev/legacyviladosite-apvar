e6f3a287eeca089944ac0a2398ce26ac52bc754d
bruckbmrj <bruckbmrj@hotmail.com>
2018-04-27 16:12:14 -0300

    inserção do "viewport"
    inserindo o meta que faltava para resolver a questão das fontes não
    estarem se adaptando ao dispositivo.

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>APVAR - Associação de pilotos da Varig</title>
 		
 		<?php wp_head(); ?>
