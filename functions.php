b12ec7385c33e55d0dc64d0e3575a4b69064844d
bruckbmrj <bruckbmrj@hotmail.com>
2018-05-04 16:17:02 -0300

    widget ouvidoria

--- functions.php
+++ functions.php
@@ -20,6 +20,13 @@ function widgets_novos_widgets_init() {
 }
 add_action( 'widgets_init', 'widgets_novos_widgets_init' );
 
+
+function ouvidoria_register_widget() {
+register_widget( 'ouvidoria_widget' );
+}
+
+add_action( 'widgets_init', 'ouvidoria_register_widget' );
+
 class ouvidoria_widget extends WP_Widget {
 	
 	function __construct() {
@@ -27,10 +34,44 @@ class ouvidoria_widget extends WP_Widget {
 	// widget ID
 	'ouvidoria_widget',
 	// widget name
-	__('Ouvidoria Sample Widget', ' ouvidoria_widget_domain'),
+	__('Ouvidoria Widget', ' ouvidoria_widget_domain'),
 	// widget description
-	array( 'description' => __( 'Ouvidoria Widget Use', 'ouvidoria_widget_domain' ), )
+	array( 'description' => __( 'Widget used to show Ouvidoria', 'ouvidoria_widget_domain' ), )
 	);
+	}
+
+	public function widget( $args, $instance ) {
+		$title = apply_filters( 'widget_title', $instance['title'] );
+		echo $args['before_widget'];
+		//if title is present
+		if ( ! empty( $title ) )
+		echo $args['before_title'] . $title . $args['after_title'];
+		//output
+		echo __( '<p class="ouvidoria-text">
+			Envie-nos suas Sugestões, Dúvidas, Reclamações etc.
+			<a href="http://www.apvar.org.br/v1/ouvidoria/">Clique aqui >>>> </a>
+			</p>' );
+
+		echo $args['after_widget'];
+	}
+
+	public function form( $instance ) {
+		if ( isset( $instance[ 'title' ] ) )
+		$title = $instance[ 'title' ];
+		else
+		$title = __( 'Ouvidoria', 'hstngr_widget_domain' );
+		?>
+		<p>
+		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		</p>
+		<?php
+	}
+
+	public function update( $new_instance, $old_instance ) {
+		$instance = array();
+		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
+		return $instance;
 	}
  
 }
