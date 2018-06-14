83506639ba8cdd7999081a1df79ec9c900bf1bce
bruckbmrj <bruckbmrj@hotmail.com>
2018-06-14 17:40:13 -0300

    incluir avião no titulo e criar paginas especificas

--- page.php
+++ page.php
@@ -9,7 +9,11 @@
 				<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
 
 					<div class="main-title col-md-12">
+						<div class="envolve-titulo">
+						<img id='logo-title' class="plane-title" src='<?php echo get_bloginfo('template_directory') . '/img/title-apvar.png'; ?>' alt='Logotipo'>
+						
 						<h2><?php the_title(); ?></h2>
+						</div>
 					</div>
 
 					<div class="separator col-md-12">
