45bb4598e887c33a248221c65614bdbdc0835588
bruckbmrj <bruckbmrj@hotmail.com>
2018-06-18 14:09:14 -0300

    criar paginas de diretoria parte 2

---
+++ page-conselho-fiscal.php
@@ -0,0 +1,55 @@
+<?php get_header(); ?>
+
+
+
+		<!-- header area -->
+		<div class="row content center-block">
+			<div class="main col-md-12">
+
+				<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+
+					<div class="main-title col-md-12">
+						<div class="envolve-titulo">
+						<img id='logo-title' class="plane-title" src='<?php echo get_bloginfo('template_directory') . '/img/title-apvar.png'; ?>' alt='Logotipo'>
+						
+						<h2><?php the_title(); ?></h2>
+						</div>
+					</div>
+
+					<div class="separator col-md-12">
+					</div>
+
+					<div class="clearfix"></div>
+
+					<div class="chamada col-md-1 col-xs-1">
+						
+						<img id='logo-title' class="plane-title" src='<?php echo get_bloginfo('template_directory') . '/img/tit_dir_cons.gif'; ?>' alt='Logotipo'>
+
+					</div>
+
+					<div class="chamada-conteudo-dir col-md-11 col-xs-11">
+						
+						<?php the_content(); ?>
+
+					</div>
+
+				<?php endwhile; else: ?>
+				<?php endif; ?>	
+					
+				<div class="clearfix"></div>
+
+					<hr>
+
+				<div class="clearfix"></div>
+
+			</div>
+
+
+			<div class="separator col-md-12">
+			</div>
+
+
+		</div>
+
+		<!-- footer area -->
+<?php get_footer(); ?>
