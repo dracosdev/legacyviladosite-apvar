3fe4f3d954a0145d1e442dfb6d3d19b44a9c1559
bruckbmrj <bruckbmrj@hotmail.com>
2018-08-03 12:49:50 -0300

    transformar o footer em row

--- footer.php
+++ footer.php
@@ -1,15 +1,12 @@
+			<footer class="footer row">
+			<div class="">
+				APVAR: Av. Franklin Roosevelt 84/302 – Castelo – RJ - CEP 20021-120 - Tel: (+55 21) 9 6406-7661
+			</div>
+
+			</footer>
 		</div>
 
 		</div>
-
-		<div class="separator col-md-12">
-		</div>
-
-		<footer class="footer col-md-12">
-			
-			APVAR: Av. Franklin Roosevelt 84/302 – Castelo – RJ - CEP 20021-120 - Tel: (+55 21) 9 6406-7661
-
-		</footer>
 
 		<?php wp_footer(); ?>
 
