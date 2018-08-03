bc64fcba5e5a6308151fe414f6de0e19addf5e41
Vila do Site <admin@viladosite.com.br>
2018-08-03 11:15:18 -0300

    Criação dos template parts para categories

--- category.php
+++ category.php
@@ -1,30 +1,13 @@
 <?php get_header(); ?>
 	<div class="row content center-block">
+		
 		<div class="main col-md-12">
-			<div class="main-title col-md-12">
-				<div class="envolve-titulo">
-					<img id='logo-title' class="plane-title" src='<?php echo get_bloginfo('template_directory') . '/img/title-apvar.png'; ?>' alt='Logotipo'>
-					<h2><?php the_category( ' ' ); ?></h2>
-				</div>
-			</div>
+		
+			<?php get_template_part('layout/titulo-categories'); ?>
+		
 			<div class="separator col-md-12"></div>
-
-			<!-- Início do loop de posts -->
-			<?php if (have_posts()) : while(have_posts()) : the_post();	?>
-				<div class="main-title col-md-9">
-					<div class="envolve-titulo">
-						<img id='logo-title' class="plane-title" src='<?php echo get_bloginfo('template_directory') . '/img/title-apvar.png'; ?>' alt='Logotipo'>
-						<h2><?php the_title(); ?></h2>
-					</div>
-				</div>
-
-				<div class="separator col-md-12"></div>
-				<div class="clearfix"></div>
-
-				<div class="chamada col-md-9"> <?php the_content(); ?> </div>
-
-			<?php endwhile; else: ?>
-			<?php endif; ?>	
+		
+			<?php get_template_part('layout/loop-categories'); ?>
 
 			<!-- Área da Sidebar -->
 			<div class="bordas">
---
+++ layout/titulo-categories.php
@@ -0,0 +1,6 @@
+<div class="main-title col-md-12">
+	<div class="envolve-titulo">
+		<img id='logo-title' class="plane-title" src='<?php echo get_bloginfo('template_directory') . '/img/title-apvar.png'; ?>' alt='Logotipo'>
+		<h2><?php the_category( ' ' ); ?></h2>
+	</div>
+</div>
