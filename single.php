ca7495b28a87273e5a549e2eebdd3c9abd9a769c
Vila do Site <admin@viladosite.com.br>
2018-08-03 11:22:32 -0300

    Remoção de conteúdo não utilizado

--- single.php
+++ single.php
@@ -1,34 +1,5 @@
 <?php get_header(); ?>
 
-<?php
-
-// Montagem dos argumentos para cada loop
-$args_apvaracao = array(
-	'cat' => 3,
-	'post_type' => 'post',
-	'posts_per_page' => 2
-);
-
-$args_quadroavisos = array(
-	'cat' => 4,
-	'post_type' => 'post',
-	'posts_per_page' => 2
-);
-
-$args_deunaimprensa = array(
-	'cat' => 5,
-	'post_type' => 'post',
-	'posts_per_page' => 3
-);
-
-
-// Montagem das mensagens pré-definidas
-$nenhumpost = '<p> Não foi encontrada nenhuma publicação nesta categoria. </p>';
-
-?>
-
-
-		<!-- header area -->
 		<div class="row content center-block">
 			<div class="main col-md-10">
 				
@@ -41,44 +12,30 @@ $nenhumpost = '<p> Não foi encontrada nenhuma publicação nesta categoria. </p
 
 				<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
 
-
 					<div class="chamada col-md-12">
 						<h2><?php the_title(); ?></h2>
 						<?php the_content(); ?>
-
 					</div>
+
 				<?php endwhile; else: ?>
 				<?php endif; ?>	
 					
-					<div class="clearfix"></div>
+				<div class="clearfix"></div>
+				<hr>
+				<div class="clearfix"></div>			
 
-					<hr>
-
-					<div class="clearfix"></div>			
 			</div>
 
 			<!-- area da sidebar -->
 			<div class="bordas">
-			<aside class="complementary col-md-2">
-
+				<aside class="complementary col-md-2">
 					<?php get_sidebar(); ?>
-					
-
-						<div class="clearfix"></div>
-
-
-
-			</aside>
+					<div class="clearfix"></div>
+				</aside>
 			</div>
 
-
-			<div class="separator col-md-12">
-			</div>	
-
+			<div class="separator col-md-12"></div>	
 			<div class="clearfix"></div>
-
-
 		</div>
 
-		<!-- footer area -->
 <?php get_footer(); ?>
