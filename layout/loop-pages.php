b78b7276d824ec24b754853824077ec9b928281a
Vila do Site <admin@viladosite.com.br>
2018-08-03 11:15:37 -0300

    Criação dos template parts para pages

---
+++ layout/loop-pages.php
@@ -0,0 +1,17 @@
+<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
+	<div class="main-title col-md-12">
+		<div class="envolve-titulo">
+			<img id='logo-title' class="plane-title" src='<?php echo get_bloginfo('template_directory') . '/img/title-apvar.png'; ?>' alt='Logotipo'>
+			
+			<h2><?php the_title(); ?></h2>
+		</div>
+	</div>
+
+	<div class="separator col-md-12"></div>
+	<div class="clearfix"></div>
+
+	<div class="envolve-base col-md-9">
+		<?php if (tem_filhos()) {pega_filhos();} else {the_content();}?>
+	</div>
+<?php endwhile; else: ?>
+<?php endif; ?>	
--- page.php
+++ page.php
@@ -1,23 +1,8 @@
 <?php get_header(); ?>
 		<div class="row content center-block">
 			<div class="main col-md-12">
-				<?php if (have_posts()) : while(have_posts()) : the_post(); ?>
-					<div class="main-title col-md-12">
-						<div class="envolve-titulo">
-							<img id='logo-title' class="plane-title" src='<?php echo get_bloginfo('template_directory') . '/img/title-apvar.png'; ?>' alt='Logotipo'>
-							
-							<h2><?php the_title(); ?></h2>
-						</div>
-					</div>
-
-					<div class="separator col-md-12"></div>
-					<div class="clearfix"></div>
-
-					<div class="envolve-base col-md-9">
-						<?php if (tem_filhos()) {pega_filhos();} else {the_content();}?>
-					</div>
-				<?php endwhile; else: ?>
-				<?php endif; ?>					
+			
+			<?php get_template_part('layout/loop-pages'); ?>
 
 				<!-- area da sidebar -->
 				<div class="bordas">
