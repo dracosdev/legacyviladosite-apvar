bab9244a62d55a6ec794793457628a6797e85382
bruckbmrj <bruckbmrj@hotmail.com>
2018-08-03 13:53:35 -0300

    criar paginas personalizadas

---
+++ page-diretoria.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+		<div class="row content center-block">
+			<div class="main col-md-12">
+			
+			<?php get_template_part('layout/loop-pages'); ?>
+
+				<div class="clearfix"></div>
+				<hr>
+				<div class="clearfix"></div>			
+			</div>
+			<div class="separator col-md-12"></div>	
+			<div class="clearfix"></div>
+		</div>
+<?php get_footer(); ?>
---
+++ page-institucional.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+		<div class="row content center-block">
+			<div class="main col-md-12">
+			
+			<?php get_template_part('layout/loop-pages'); ?>
+
+				<!-- area da sidebar -->
+				<div class="bordas">
+					<aside class="complementary col-md-3">
+
+						<?php if(is_active_sidebar('lat_right_institucional_widgets')){
+				            dynamic_sidebar('lat_right_institucional_widgets');
+		    			}; ?>
+						<div class="clearfix"></div>
+					</aside>
+				</div>
+				<div class="clearfix"></div>
+				<hr>
+				<div class="clearfix"></div>			
+			</div>
+			<div class="separator col-md-12"></div>	
+			<div class="clearfix"></div>
+		</div>
+<?php get_footer(); ?>
