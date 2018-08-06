79a6897d6b65366336bfb81b3218b88698477f33
Vila do Site <admin@viladosite.com.br>
2018-08-06 09:42:30 -0300

    Criação lo loop de categoria "lastpost"
    A página de loop de categoria lastpost gera um loop da categoria em exibição no momento porem exibe apenas o post mais recente desta categoria.
    
    Este modelo foi criado para ser usado em páginas em conjunto com o widget "Posts por categoria"

---
+++ layout/loop-categories-lastpost.php
@@ -0,0 +1,35 @@
+<!-- Início do loop de posts -->
+<?php
+
+// Variáveis
+$category = get_category( get_query_var( 'cat' ) );
+$cat_id = $category->cat_ID;
+
+// Argumentos
+$args = array(
+    'posts_per_page' => 1,
+    'cat' => $cat_id    
+);
+
+// Loop
+query_posts($args);
+
+if (have_posts()) : while(have_posts()) : the_post();	?>
+	
+	<div class="chamada col-md-12">
+	
+		<div class="envolve-titulo">
+			<img id='logo-title' class="plane-title" src='<?php echo get_bloginfo('template_directory') . '/img/title-apvar.png'; ?>' alt='Logotipo'>
+			<h2><?php the_title(); ?></h2>
+		</div>
+
+		<div> <?php the_content(); ?> </div>
+
+	</div>
+
+<?php
+endwhile;
+else:
+
+?>
+<?php endif; ?>	
