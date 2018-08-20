9b22a0e70c5da8f0a462fa04b049190cf2a9808d
bruckbmrj <bruckbmrj@hotmail.com>
2018-08-20 12:40:35 -0300

    retornar loop geral

--- layout/loop-geral.php
+++ layout/loop-geral.php
@@ -3,14 +3,11 @@
 	
 	<div class="chamada col-md-12">
 	
-		<a href="<?php echo esc_url( get_permalink($post->ID)); ?>">
-			<div class="envolve-titulo envolve-titulo-interno">
-				<h2><?php the_title(); ?></h2>
-			</div>
-			<div>
-				<?php the_excerpt(); ?>	
-			</div>
-		</a>
+		<div class="envolve-titulo envolve-titulo-interno">
+			<h2><?php the_title(); ?></h2>
+		</div>
+
+		<div> <?php the_content(); ?> </div>
 
 	</div>
 
