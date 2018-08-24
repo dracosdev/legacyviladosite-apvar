db17230d62c02ff5700d62113ebe5721fec6bb2c
bruckbmrj <bruckbmrj@hotmail.com>
2018-08-24 11:24:54 -0300

    criar menu retrátil

--- header.php
+++ header.php
@@ -19,22 +19,33 @@
 
 				<a href='<?php echo home_url(); ?>'>
 				<img id='logotipo' src='<?php echo get_bloginfo('template_directory') . '/img/apvar-logo.png'; ?>' alt='Logotipo'>
-				</a>
+				</a>	
 
 				<nav class="navbar navbar-default menu-barra">
+			    <!-- Brand and toggle get grouped for better mobile display -->
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#menu-principal" aria-expanded="false">
+			        <span class="sr-only">Toggle navigation</span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			      </button>
+			    </div>
 
-					<?php 
+			    <!-- Collect the nav links, forms, and other content for toggling -->
+			    <div class="collapse navbar-collapse" id="menu-principal">
+			      	<?php 
 
-					wp_nav_menu( array(
+						wp_nav_menu( array(
 						'menu' => 'menu_princ',
 						'theme_location' => 'menu_princ',
 						'container' => 'nav',
 						'items_wrap' => '<ul id="%1$s" class="nav navbar-nav %2$s">%3$s</ul>'
 					) );
-					 ?>
-
-
-				</nav>		 
+					?>
+			 
+			    </div><!-- /.navbar-collapse -->
+				</nav>	 
 
 
 
