7199fbf03035a8d4ec410701b9bc653fb5d239fa
Vila do Site <admin@viladosite.com.br>
2018-09-14 13:31:39 -0300

    Adição do campo matrícula
    Adição do campo pessoal "Matricula" nos perfis de usuários.

--- functions.php
+++ functions.php
@@ -547,10 +547,6 @@ class falsa_recup_widget extends WP_Widget {
 
 
 
-
-
-
-
 // Registra e inicializa os widgets
 function register_widgets() {
 	
@@ -561,6 +557,49 @@ function register_widgets() {
 }
 
 add_action( 'widgets_init', 'register_widgets' );
+
+
+
+/*-----------------------------------------------------------------------------------*/
+/* CAMPOS ADICIONAIS
+/*-----------------------------------------------------------------------------------*/
+
+// CAMPOS DE PERFIL PERSONALIZADOS
+add_action( 'show_user_profile', 'my_show_extra_profile_fields' );
+add_action( 'edit_user_profile', 'my_show_extra_profile_fields' );
+ 
+function my_show_extra_profile_fields( $user ) { ?>
+
+
+    <table class="form-table">
+ 
+        <tr>
+            <th><label for="matricula">Matricula</label></th>
+ 
+            <td>
+                <input type="text" name="matricula" id="matricula" value="<?php echo esc_attr( get_the_author_meta( 'matricula', $user->ID ) ); ?>" class="regular-text" /><br />
+                <span class="description">Matrícula do usuário</span>
+            </td>
+        </tr>
+
+    </table>  
+<?php } ?>
+
+<?php
+
+
+// GUARDAR E MANTER INFO DOS CAMPOS
+add_action( 'personal_options_update', 'my_save_extra_profile_fields' );
+add_action( 'edit_user_profile_update', 'my_save_extra_profile_fields' );
+ 
+function my_save_extra_profile_fields( $user_id ) {
+ 
+    if ( !current_user_can( 'edit_user', $user_id ) )
+        return false;
+    update_usermeta( $user_id, 'matricula', $_POST['matricula'] );
+}
+
+
 
 
  ?>
