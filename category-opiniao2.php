bea5befbfbf9169022c21b06e58376098086ee82
bruckbmrj <bruckbmrj@hotmail.com>
2018-10-26 14:41:10 -0300

    criar layout para categoria opinião

---
+++ category-opiniao2.php
@@ -0,0 +1,39 @@
+<?php get_header();
+
+// Define o nome da sidebar usada nessa página
+// Deixar vazio caso não queira sidebar nesta página
+$sidebar_name = 'lat_right_categories_widgets'; ?>
+		<div class="row content center-block">
+			<div class="main col-md-12">
+				<?php get_template_part('layout/titulo-geral');
+
+				// Função que abre a div de coluna variando de acordo com apresença da sidebar
+				if (is_active_sidebar($sidebar_name)) {echo '<div class="col-md-9">';}
+				else {echo '<div id="conteudo" class="col-md-12">';};
+
+					get_template_part('layout/loop-geral-lastpost');
+
+					echo "<div class='col-md-12 center-block paginacao'> ";
+					wordpress_pagination();
+					echo "</div>";
+
+				echo "</div>";
+				
+				?>
+
+				<!-- area da sidebar -->
+				<div class="bordas">
+					<aside class="complementary col-md-3">
+						<?php if(is_active_sidebar($sidebar_name)){
+				            dynamic_sidebar($sidebar_name);
+		    			}; ?>
+						<div class="clearfix"></div>
+					</aside>
+				</div>
+				<div class="clearfix"></div>
+				<hr>
+				<div class="clearfix"></div>			
+			</div>
+			<?php bs_separator(); ?>
+		</div>
+<?php get_footer(); ?>
